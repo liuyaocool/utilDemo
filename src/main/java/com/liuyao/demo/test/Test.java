e6303e143e36307c523466978ab307ed8d7c0496
liuyao <liuyao_coolman@163,com>
2018-10-16 17:09:11 +0800

    旋转图标

---
+++ src/main/java/com/liuyao/demo/test/Test.java
@@ -0,0 +1,15 @@
+package com.liuyao.demo.test;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(new Double("12.1").intValue());
+        System.out.println(new Double("12.1").toString());
+        System.out.println(new Double("12.0").intValue());
+        System.out.println(12.0%1 == 0);
+        System.out.println(12.1%1 == 0);
+
+    }
+}
