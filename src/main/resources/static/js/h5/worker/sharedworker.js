44d71ca276e07ded4e1688eb000c1d6172106cb1
liuyao <zl_liuy@163.com>
2018-12-06 15:00:53 +0800

    sharedworker

--- src/main/resources/static/js/h5/worker/sharedworker.js
+++ src/main/resources/static/js/h5/worker/sharedworker.js
@@ -3,4 +3,16 @@
 onconnect = function (ev) {
     var port = ev.ports[0];
     port.postMessage("work");
-}
+};
+
+onmessage = function (ev) {
+    var a = ev.data.a;
+    var b = ev.data.b;
+    var c = ev.data.c;
+    // postMessage("a:" +a + "/b:" + b + "/c:" + c);
+    console.log("send.js");
+    postMessage({
+        a: a,
+        b: b
+    });
+};
