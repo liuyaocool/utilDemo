fc4f13685b6237b806875b78f783d9ee919544bd
liuyao <zl_liuy@163.com>
2018-12-06 15:59:05 +0800

    sharedworker-sendmsg success

--- src/main/resources/static/js/h5/worker/sharedworker.js
+++ src/main/resources/static/js/h5/worker/sharedworker.js
@@ -1,18 +1,19 @@
 
 //标识当共享子线程与主线程建立连时出发的事件
 onconnect = function (ev) {
-    var port = ev.ports[0];
-    port.postMessage("work");
+    var port0 = ev.ports[0];
+    port0.postMessage("connect sharedwork success");
+    port0.onmessage = onMsg;
 };
 
-onmessage = function (ev) {
+function onMsg(ev) {
     var a = ev.data.a;
     var b = ev.data.b;
     var c = ev.data.c;
     // postMessage("a:" +a + "/b:" + b + "/c:" + c);
     console.log("send.js");
-    postMessage({
+    this.postMessage({
         a: a,
         b: b
     });
-};
+}
