5288a2d852b19320dbe4096e3a868c01dec80262
liuyao <zl_liuy@163.com>
2019-03-04 09:13:57 +0800

    timeout unicode

---
+++ src/main/java/com/liuyao/demo/util/UncodeAndChar.java
@@ -0,0 +1,24 @@
+package com.liuyao.demo.util;
+
+public class UncodeAndChar {
+
+	public static void main(String[] args) {
+		String aa = null;
+		System.out.println("null".equals(String.valueOf(aa)));
+		System.out.println(4 ^ 3);
+		System.out.println(2 ^ 5);
+		System.out.println((2 ^ 5) << 4);
+		System.out.println(7 << 4);
+		System.out.println((2 ^ 5) << 4 ^ 3);
+	}
+
+	public static String toChar(String unicode){
+
+		return "";
+	}
+
+	public static String toUnicode(char c){
+
+		return "";
+	}
+}
