6b64cec1ff2ed5b588ec86c9d2dd0723f255d48f
liuyao <zl_liuy@163.com>
2019-03-06 17:40:47 +0800

    T

--- src/main/java/com/liuyao/demo/test/study/TestTDemo.java
+++ src/main/java/com/liuyao/demo/test/study/TestTDemo.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class TestDemo{
+public class TestTDemo{
 	
 	public static void main(String[] args) {
 		Test_T_fanXing<Integer> inte = new Test_T_fanXing<Integer>(77);
