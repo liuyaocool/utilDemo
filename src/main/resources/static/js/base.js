318cc94e0c5b73e0f15b7251bb9adf034d328141
liuyao <liuyao@riverroad.cn>
2019-08-18 04:24:24 +0800

    获得渐变色

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -200,3 +200,51 @@ function dateFormat(date, day, format) {
     format = format.replace("ss", ss < 10 ? "0" + ss : ss);
     return format;
 }
+
+
+function rgbToHex(r, g, b){
+    var hex = ((r<<16) | (g<<8) | b).toString(16);
+    return "#" + new Array(Math.abs(hex.length-7)).join("0") + hex;
+}
+
+// hex to rgb
+function hexToRgb(hex){
+    var rgb = [];
+    for(var i=1; i<7; i+=2){
+        rgb.push(parseInt("0x" + hex.slice(i,i+2)));
+    }
+    return rgb;
+}
+
+/**
+ * 计算渐变过渡色('#ec9089', '#c12927', 100, 24) 取100个:[) 第24个
+ */
+function gradient (startColor,endColor,step, index){
+    // 将 hex 转换为rgb
+    var sColor = hexToRgb(startColor),
+        eColor = hexToRgb(endColor);
+    // 计算R\G\B每一步的差值
+    var rStep = (eColor[0] - sColor[0]) / step,
+        gStep = (eColor[1] - sColor[1]) / step,
+        bStep = (eColor[2] - sColor[2]) / step;
+    //若确定取第几个
+    if ((index  = parseInt(index)) > 0) {
+        return hex(index);
+    }
+    //获得结果集
+    var gradientColorArr = [];
+    for(var i = 0; i < step; i++){
+        // 计算每一步的hex值
+        gradientColorArr.push(hex(i));
+    }
+    return gradientColorArr;
+
+    //创建16进制坐标
+    function hex(num) {
+        return rgbToHex(
+            parseInt(rStep * num + sColor[0]),
+            parseInt(gStep * num + sColor[1]),
+            parseInt(bStep * num + sColor[2])
+        )
+    }
+}
