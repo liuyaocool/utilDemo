afd746328fa065887dab3198e109389d6ac8a604
刘耀 <zl_liuy@163.com>
2021-02-06 09:12:32 +0800

    笔记 改为markdown

--- src/main/java/com/liuyao/demo/utilutil/Test.java
+++ src/main/java/com/liuyao/demo/utilutil/Test.java
@@ -6,6 +6,8 @@ public class Test {
 
     public static void main(String[] args) {
 
+        System.out.println("aaa" == "aaa");
+
         for (int i = 0; i < 3; i++) {
             Scanner sc = new Scanner(System.in);
             sc.hasNext();
